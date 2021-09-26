9e576a6fb5209bc71c111780111e475a774f5b35
Avish Kumar <85635883+aviskumar@users.noreply.github.com>
2021-09-26 21:13:28 +0530

    Add files via upload

---
+++ database/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2020-2021 by TeamSpeedo@Github, < https://github.com/TeamSpeedo >.
+#
+# This file is part of < https://github.com/TeamSpeedo/SpeedoUserBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamSpeedo/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import logging
+
+from main_start import mongo_client
+from main_start.config_var import Config
+
+db_x = mongo_client["Speedo"]
